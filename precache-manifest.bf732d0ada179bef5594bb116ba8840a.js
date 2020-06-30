f0e606361f6ba18669f7963eaf43882c23a82006
Shermann <animeobzor.shamann@gmail.com>
2020-07-01 02:18:23 +0300

    Updates

---
+++ precache-manifest.bf732d0ada179bef5594bb116ba8840a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c35bb93eb4b01097f6d38f59a2030a",
+    "url": "/malendar/index.html"
+  },
+  {
+    "revision": "7dd4fdd1ae773d4bc7c9",
+    "url": "/malendar/static/css/main.ac45e67e.chunk.css"
+  },
+  {
+    "revision": "8d641a3bcd8599c4caf6",
+    "url": "/malendar/static/js/2.c85fbf71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/malendar/static/js/2.c85fbf71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd4fdd1ae773d4bc7c9",
+    "url": "/malendar/static/js/main.ee2eebd6.chunk.js"
+  },
+  {
+    "revision": "123418e687fc7ea567f8",
+    "url": "/malendar/static/js/runtime-main.ed17ac73.js"
+  }
+]);
