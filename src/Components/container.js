75a31659f90d2d483855c5a6caf112a1f9f6ba4c
Shermann <animeobzor.shamann@gmail.com>
2020-07-01 02:47:33 +0300

    Updated UI

--- src/Components/container.js
+++ src/Components/container.js
@@ -1,75 +1,95 @@
 import React, {useEffect, useState} from "react";
 import "./container.css";
 import Day from "./day";
-import animeScissors, {titlelist} from "../api/getanime";
+import animeScissors, {getLastDayOfMonth, titlelist} from "../api/getanime";
 
 const Container = () => {
-    const [titles, setTitles] = useState([titlelist]);
-    const [month, setMonth] = useState('July');
-    const [year, setYear] = useState(2020);
+  const [titles, setTitles] = useState([titlelist]);
+  const [month, setMonth] = useState("February");
+  const [year, setYear] = useState(2020);
 
     useEffect(() => {
-        (async function xd() {
-                console.log(`year: ${year} and month: ${month}`);
-                await animeScissors(year, getSeasonByMonth(month), month);
-                //console.log('Titlelist is ', titlelist);
-                console.log('titlelist had updated!');
-                await setTitles(titles => titles = [...titlelist]);
-                //await getLargeImages();
-                //console.log('Titlelist comparison: ', titles, typeof titles);
-            }
-        )();
+      (async function xd() {
+        console.log(`year: ${year} and month: ${month}`);
+        await animeScissors(year, getSeasonByMonth(month), month);
+        //console.log('Titlelist is ', titlelist);
+        console.log("titlelist had updated!");
+        await setTitles((titles) => (titles = [...titlelist]));
+        //await getLargeImages();
+        //console.log('Titlelist comparison: ', titles, typeof titles);
+      })();
     }, [month, year]);
 
     const getSeasonByMonth = (month) => {
-        let season = '';
-        if (month === 'January' || month === 'February') {
-            season = 'winter';
-        } else if (month === 'March' || month === 'April' || month === 'May') {
-            season = 'spring'
-        } else if (month === 'June' || month === 'July' || month === 'August') {
-            season = 'summer'
-        } else season = 'fall';
+      let season = "";
+      if (month === "January" || month === "February") {
+        season = "winter";
+      } else if (month === "March" || month === "April" || month === "May") {
+        season = "spring";
+      } else if (month === "June" || month === "July" || month === "August") {
+        season = "summer";
+      } else season = "fall";
 
-        return season;
-    }
+      return season;
+    };
 
     const monthClick = () => {
-        let months = ['January', 'February',
-            'March', 'April', 'May',
-            'June', 'July', 'August',
-            'September', 'October', 'November', 'December'];
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
 
-        setMonth(month => month = months[Math.ceil(Math.random() * 11)]);
-        console.log('Current season is', getSeasonByMonth(month));
-    }
+      setMonth((month) => (month = months[Math.ceil(Math.random() * 11)]));
+      console.log("Current season is", getSeasonByMonth(month));
+    };
 
-    const yearClick = () => {
-        setYear(year => year = Math.ceil(Math.random() * 20) + 2000);
-    }
+  const yearClick = () => {
+    setYear((year) => (year = Math.ceil(Math.random() * 20) + 2000));
+  };
 
     return (
-        <main className='Container'>
-            <div className='Head-Container'>
-                <div className='Date-nav'>
-                    <div className='dateMonth padding-left' onClick={monthClick}>{month}</div>
-                    <div className='dateYear padding-right' onClick={yearClick}>{year}</div>
-                </div>
-                <div className='Release-nav'>
-                    <div className='padding-left'>Ongoing</div>
-                    <div className='padding-right active'>New releases</div>
-                </div>
+        <main className="Container">
+          <div className="Head-Container">
+            <div className="Date-nav">
+              <div className="dateMonth padding-left" onClick={monthClick}>
+                {month}
+              </div>
+              <div className="dateYear padding-right" onClick={yearClick}>
+                {year}
+              </div>
             </div>
-
-            <div className='Days'>
-                {titles.length > 0 ? titles.map((el, i) =>
-                    <Day id={(i + 1).toString().padStart(2, '0')} key={i} ani={titles[i]}/>
-                ) : <div className='simpleText'>There is no data on this period</div>}
-
-
+            <div className="Release-nav">
+              <div className="padding-left">Ongoing</div>
+              <div className="padding-right active">New releases</div>
             </div>
+          </div>
+
+          <div className="Days">
+            {titles.length > 0 ? (
+                titles.map((el, i) => (
+                    <Day
+                        id={(i + 1).toString().padStart(2, "0")}
+                        key={i}
+                        ani={titles[i]}
+                        ldm={getLastDayOfMonth(month, year)}
+                    />
+                ))
+            ) : (
+                <div className="simpleText">There is no data on this period</div>
+            )}
+          </div>
         </main>
     );
-}
+};
 
 export default Container;
--- src/api/getanime.js
+++ src/api/getanime.js
@@ -1,27 +1,27 @@
-const API = 'https://private-anon-98ba329006-jikan.apiary-proxy.com/v3/';
+const API = "https://private-anon-98ba329006-jikan.apiary-proxy.com/v3/";
+//const API = "https://api.jikan.moe/v3/";
 
 export let titlelist = [];
 
 const getSeason = async (year, season) => {
-    const response = await fetch(`${API}season/${year}/${season}/`)
-        //const response = await fetch(`${API}season/`)
-        .then(res => {
-                //console.log(res);
-                if (res.status === 429) {
-                    console.log('Too many requests!');
-                }
-                //console.log('response is ', res);
-                if (!res.ok) {
-                    console.error("Could not fetch!");
-                }
-                return res;
-            }
-        );
-    //.catch(error => console.error('error blin'));
+  const response = await fetch(`${API}season/${year}/${season}/`)
+      //const response = await fetch(`${API}season/`)
+      .then((res) => {
+        //console.log(res);
+        if (res.status === 429) {
+          console.log("Too many requests!");
+        }
+        //console.log('response is ', res);
+        if (!res.ok) {
+          console.error("Could not fetch!");
+        }
+        return res;
+      });
+  //.catch(error => console.error('error blin'));
 
-    const resJSON = await response.json();
-    return resJSON['anime'];
-}
+  const resJSON = await response.json();
+  return resJSON["anime"];
+};
 /*let images = [];*/
 
 /*const imageSaver = (image, el) => {
@@ -62,90 +62,102 @@ const getAnimelist = (anime, month, year) => {
     animelist = animelist[0];
 
     for (const el in animelist) {
-        animelist[el] =
-            {
-                id: animelist[el].mal_id,
-                title: animelist[el].title,
-                airing: animelist[el].airing_start,
-                image: animelist[el].image_url,
-                studio: animelist[el].producers[0],
-            };
+      animelist[el] = {
+        id: animelist[el].mal_id,
+        title: animelist[el].title,
+        airing: animelist[el].airing_start,
+        image: animelist[el].image_url,
+        studio: animelist[el].producers[0],
+      };
     }
-    ;
-    titlelist = animelist;
-    sortAnime();
-    getAnimeByMonth(month, year);
+  titlelist = animelist;
+  sortAnime();
+  getAnimeByMonth(month, year);
 
-    //console.log(titlelist);
-    return true;
-}
+  //console.log(titlelist);
+  return true;
+};
 
 const sortAnime = () => {
     //console.log('sort has started!', titlelist);
     let animelist = [...titlelist];
 
-    animelist.sort((a, b) => {
-        if (a.airing > b.airing) {
-            return 1;
-        } else if (a.airing < b.airing) {
-            return -1;
-        } else return 0;
-    })
+  animelist.sort((a, b) => {
+    if (a.airing > b.airing) {
+      return 1;
+    } else if (a.airing < b.airing) {
+      return -1;
+    } else return 0;
+  });
 
-    titlelist = [...animelist];
-    //console.log('sort is finished!', titlelist);
-}
+  titlelist = [...animelist];
+  //console.log('sort is finished!', titlelist);
+};
+
+export const getLastDayOfMonth = (month, year) => {
+  return new Date(
+      year,
+      new Date(`01 ${month} ${year}`).getMonth() + 1,
+      0
+  ).getDate();
+};
 
 const getAnimeByMonth = (month, year) => {
-    let animelist = [...titlelist];
-    let newlist = [];
-    newlist.length = new Date(year, new Date(`01 ${month} ${year}`).getMonth() + 1, 0).getDate();
-    newlist = fillNewList(newlist, month, year);
-    console.log('newlist is', newlist);
+  let animelist = [...titlelist];
+  let newlist = [];
+  newlist.length = getLastDayOfMonth(month, year);
+  newlist.length = 35;
+  newlist = fillNewList(newlist, month, year);
+  console.log("newlist is", newlist);
 
-    animelist.forEach((a, index) => {
-            if ((new Date(a.airing).getTime() <= new Date(`31 ${month} ${year}`).getTime()
-                && new Date(a.airing).getTime() >= new Date(`01 ${month} ${year}`).getTime())) {
-                //console.log(a, 'a.airing is true');
-                a.day = new Date(a.airing).getDate()
-                newlist[a.day - 1] = a;
-                return true;
-            }
-            return false;
-        }
-    );
+  animelist.forEach((a, index) => {
+    if (
+        new Date(a.airing).getTime() <=
+        new Date(`31 ${month} ${year}`).getTime() &&
+        new Date(a.airing).getTime() >= new Date(`01 ${month} ${year}`).getTime()
+    ) {
+      //console.log(a, 'a.airing is true');
+      a.day = new Date(a.airing).getDate();
+      newlist[a.day - 1] = a;
+      return true;
+    }
+    return false;
+  });
 
-    console.log('animelist after gabm is', newlist);
-    titlelist = [...newlist];
-
-}
+  console.log("animelist after gabm is", newlist);
+  titlelist = [...newlist];
+};
 
 const fillNewList = (newlist, month, year) => {
-    let dumbTitle = {
-        id: '',
-        title: '',
-        airing: '',
-        image: '',
-        studio: {name: ''},
-    }
+  let dumbTitle = {
+    id: "",
+    title: "",
+    airing: "",
+    image: "",
+    studio: {name: ""},
+  };
 
-    let animelist = [...newlist];
-    let lastDayOfMonth = new Date(year, new Date(`01 ${month} ${year}`).getMonth() + 1, 0).getDate();
-    animelist.fill(dumbTitle, 0, lastDayOfMonth);
-    console.log('Filled list is ', animelist, '\nlast day of month is ', lastDayOfMonth);
-    return animelist;
-}
+  let animelist = [...newlist];
+  animelist.fill(dumbTitle, 0, animelist.length);
+  console.log(
+      "Filled list is ",
+      animelist
+      //"\nlast day of month is ",
+      //lastDayOfMonth
+  );
+  return animelist;
+};
 
 const animeScissors = async (year, season, month) => {
-    console.log(`${API}season/${year}/${season}/`);
-    const animeJSON = await getSeason(year, season);
-    //console.log(animeJSON);
-    //let result = await getAnimelist(animeJSON);
-    return (await getAnimelist(animeJSON, month, year));
-}
+  console.log(`${API}season/${year}/${season}/`);
+  const animeJSON = await getSeason(year, season);
+  //console.log(animeJSON);
+  //let result = await getAnimelist(animeJSON);
+  return await getAnimelist(animeJSON, month, year);
+};
 
 const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
 export default animeScissors;
